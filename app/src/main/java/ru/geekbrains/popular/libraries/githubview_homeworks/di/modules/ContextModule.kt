583fb14b9d5fc2d6c39a56c22254291a5d3dd688
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-12-22 15:55:12 +0300

    8 ДЗ: Выполнение 1 пункта ДЗ
    В модуле "ContextModule" выделены два метода "context()" и "app()".
    
    Ссылка на репозиторий:
    https://github.com/AVKhakhalin/GitHubView_HomeWorks

--- app/src/main/java/ru/geekbrains/popular/libraries/githubview_homeworks/di/modules/ContextModule.kt
+++ app/src/main/java/ru/geekbrains/popular/libraries/githubview_homeworks/di/modules/ContextModule.kt
@@ -1,17 +1,23 @@
 package ru.geekbrains.popular.libraries.githubview_homeworks.di.modules
 
-import android.app.Application
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import ru.geekbrains.popular.libraries.githubview_homeworks.App
 import javax.inject.Singleton
 
 @Module
-class ContextModule(private val app: Application) {
+class ContextModule(private val app: App) {
 
     @Singleton
     @Provides
-    fun app(): Context {
+    fun context(): Context {
+        return app
+    }
+
+    @Singleton
+    @Provides
+    fun app(): App {
         return app
     }
 }
